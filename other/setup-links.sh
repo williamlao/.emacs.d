320873cea440f42ed8ba6740925e742eabe88453
William Lao <will.lao@btcchina.com>
2014-11-28 01:23:10 +0800

    adding setup links

---
+++ other/setup-links.sh
@@ -0,0 +1,6 @@
+ln -s ~/.emacs.d/other/.bashrc ~/.bashrc
+ln -s ~/.emacs.d/.emacs ~/.emacs
+ln -s ~/Dropbox/users/will/.gitconfig ~/.gitconfig
+ln -s ~/.emacs.d/other/.jsbeautifyrc ~/.jsbeautifyrc
+ln -s ~/.emacs.d/other/.profile ~/.profile
+ln -s ~/.emacs.d/other/.screenrc ~/.screenrc
