438a3121d0c1d3624e486e4ae13c19f13f06616f
William Lao <lao.will@gmail.com>
2014-12-08 14:22:55 +0800

    adding ret carrige

--- other/setup-links.sh
+++ other/setup-links.sh
@@ -4,3 +4,4 @@ ln -s ~/Dropbox/users/will/.gitconfig ~/.gitconfig
 ln -s ~/.emacs.d/other/.jsbeautifyrc ~/.jsbeautifyrc
 ln -s ~/.emacs.d/other/.profile ~/.profile
 ln -s ~/.emacs.d/other/.screenrc ~/.screenrc
+
